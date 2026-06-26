3abdfa3f395f81ef74f45abaad3ca1a26161e7e1
Dwi Ahmad Reski <ahmadreski2712@gmail.com>
2026-06-26 11:16:35 +0700

    Ganti nama

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -15,15 +15,16 @@ class HandleInertiaRequests extends Middleware
     }
 
     public function share(Request $request): array
-    {
-        return array_merge(parent::share($request), [
-            'auth' => [
-                'user' => $request->user(),
-            ],
-            'flash' => [
-                'success' => $request->session()->get('success'),
-                'error' => $request->session()->get('error'),
-            ],
-        ]);
-    }
+{
+    return array_merge(parent::share($request), [
+        'appName' => config('app.name'),
+        'auth' => [
+            'user' => $request->user(),
+        ],
+        'flash' => [
+            'success' => $request->session()->get('success'),
+            'error' => $request->session()->get('error'),
+        ],
+    ]);
+}
 }
